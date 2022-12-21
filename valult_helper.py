404a51eabba185bbff0e6007cdb08f9e2e2bfa88
dannielshalev <44364077+dannielshalev@users.noreply.github.com>
2022-12-21 12:00:36 +0200

    Update valult_helper.py

--- valult_helper.py
+++ valult_helper.py
@@ -3,5 +3,5 @@ import os
 
 
 def get_vault_secrets(path):
-    client = hvac.Client(url='http://automotive-vault:8200/', token=os.environ['VAULT_TOKEN'])
+    client = hvac.Client(token=os.environ['VAULT_TOKEN'])
     return client.secrets._kv.v2.read_secret_version(path=path, mount_point='infraKeys')['data']['data']
