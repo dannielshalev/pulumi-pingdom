5a43e20fa9c21ae1a4bad1b68e9024a16911ea1e
dannielshalev <44364077+dannielshalev@users.noreply.github.com>
2023-05-19 00:38:58 +0300

    Update valult_helper.py

--- valult_helper.py
+++ valult_helper.py
@@ -4,4 +4,4 @@ import hvac
 
 def get_vault_secrets(path):
     client = hvac.Client(token=os.environ['VAULT_TOKEN'])
-    return client.secrets._kv.v2.read_secret_version(path=path, mount_point='infraKeys')['data']['data']
+    return client.secrets._kv.v2.read_secret_version(path=path, mount_point='testkeys')['data']['data']
